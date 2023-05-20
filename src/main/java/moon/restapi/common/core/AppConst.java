e7bd9db3e6f2d9b346af82792df0f8c65ac450b0
Mun Kyoung-min <mun9659@naver.com>
2023-05-21 00:21:06 +0900

    init2

---
+++ src/main/java/moon/restapi/common/core/AppConst.java
@@ -0,0 +1,9 @@
+package moon.restapi.common.core;
+
+public class AppConst {
+    
+    private static final String AUTHORIZTION_HEADER = "Authorization";
+
+    private static final String AUTHORITIES_KEY = "auth";
+
+}
